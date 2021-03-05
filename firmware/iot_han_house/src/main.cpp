059be385892250fa33f00d652224a2e8189b7f96
Dadigno <daviderng@gmail.com>
2021-03-05 21:48:49 +0100

    platformio project created

---
+++ firmware/iot_han_house/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+#include <semilimes.h>
+#include "secret.h" // import wifi credentials, token and channel ID
+
+
+semilimes semilimes;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.println(myToken);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
